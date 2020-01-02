18b91abece40d0ffc0930db2e7fa63ed64f9d2fc
Yurij2015 <nabor_2008@i.ua>
2020-01-02 03:55:37 +0300

    accessContro add

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -54,12 +54,12 @@ AppAsset::register($this);
 
 
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                ['label' => 'Войти', 'url' => ['/site/login']]
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post')
                 . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    'Выйти (' . Yii::$app->user->identity->username . ')',
                     ['class' => 'btn btn-link logout']
                 )
                 . Html::endForm()
